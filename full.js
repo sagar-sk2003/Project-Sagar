250484d13f59bd47093031e25cc55a3a70e00f3e
sagar-sk2003 <131658581+sagar-sk2003@users.noreply.github.com>
2024-07-08 18:04:01 +0530

    Add files via upload

---
+++ full.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Example: Form validation
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Perform validation here, e.g., check for empty fields, valid email format
+    // Display success or error message based on validation
+    alert('Form submitted!');
+});
